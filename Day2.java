c869c1e37be6ae16040c434658433c45084a6347
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-09 01:06:12 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,22 @@
+package Rifky;
+
+public class output
+
+public static void main (String [] args) {
+
+ //  materi print, println, printf
+
+ // print
+ System.out.print ("Halo People"); //digunakan untuk mencetak "Halo People" tanpa menambahkan baris output
+  
+ // println
+ System.out.println ("Halo People"); //digunakan untuk mencetak "Halo People" dan menambakan baris output 
+
+ //printf 
+ // %s untuk string, %d untuk integer, %f untuk floating point, %.2f untuk double
+ //digunakan mencetak output dengan format tertentu
+ String nama = "Muhammad Rifky Ridwan";
+ int umur = 20;
+
+ System.out.printf ("nama saya %s, umur saya %d, nama, umur);
+ 
