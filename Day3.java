0043f2c350197b35190e753d597e570f6d697c79
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-10 00:50:53 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,16 @@
+package Rifky
+
+public class komentar {
+public static main void (String [] args) {
+
+//materi contoh penggunaan komentar
+  /* (komentar digunakan untuk menambahkan catatan pada program kita.) 
+     (komentar tidak akan memengaruhi hasil program kita.)
+     (Ada dua macam tipe komentar : yang pertama menggunakan dua garis miring
+                                  : yang kedua menggunakan garis miring dan bintang */
+                                 
+  
+  // dua garis miring digunakan untuk komentar 1 baris
+  /* garis miring dan bintang digunakan untuk komentar multi baris*/ 
+  }
+}
