ad7fd5284cf9df0e750bb890355a79e9ce2a3fea
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-13 00:20:29 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,26 @@
+package Rifky;
+public class FloatingPoint {
+
+public static void main (String[] args);
+
+// *Floating point*
+/*Floating point adalah tipe data yang digunakan 
+  untuk menyimpan angka desimal yaitu float dan double*/
+
+/*float menggunakan 32-bit untuk penyimpanan dengan presisi yang lebih kecil, 
+  sedangkan double menggunakan 64-bit untuk presisi yang lebih tinggi. Keduanya
+  menggunakan tanda titik sebagai pemisah antara bilangan bulat dam pecahan. */
+
+float nilaiFloat = 3.14f; // Deklarasi variabel float
+double nilaiDouble = 3.1415926535; // Deklarasi variabel double
+
+System.out.println(nilaiFloat);
+System.out.println(nilaiDouble);
+
+}
+
+}
+
+
+
+
