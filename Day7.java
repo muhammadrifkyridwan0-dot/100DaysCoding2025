661a324846a7ba9f5a10eaa33d07874244f9b83e
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-14 06:22:25 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,30 @@
+package Rifky;
+public class TipeDataKarakter {
+  public static main void (String[] args) {
+
+    /*Tipe Data Karakter char dan boolean*/
+
+    /*Tipe data char adalah tipe data yang digunakan untuk mempresentasikan
+    karakter tunggal, seperti huruf, angka, atau simbol*/
+    
+    // contoh:
+    
+    char a = 'A'; //nilai char menggunakan tanda petik 1 (') 
+    System.out.println(a); //akan mencetak nilai (A) 
+
+    /*Tipe data boolean adalah tipe data yang digunakan untuk mempresentasikan nilai kebenaran, 
+      yaitu true (benar) atau salah (false)*/
+
+    //contoh: 
+
+    boolean benar = true;
+    boolean salah = false;
+
+    System.out.println(benar); //akan mencetak nilai true
+    System.out.println(salah); //akan mencetak nilai false
+
+  }
+
+}
+    
+   
