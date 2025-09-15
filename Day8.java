cc52552d927f879302cdf7203506a9f579fc3ccc
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-15 11:29:48 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,19 @@
+package Rifky;
+public class TipeDataString {
+
+public static main void (String[] args) {
+
+  /*Tipe data String digunakan untuk merepresentasikan teks atau karakter dalam pemrograman.
+    String dapat berisi huruf, angka, simbol, atau kombinasi dari semuanya*/
+
+  String nama = "Muhammad Rifky Ridwan";
+  String makananFavorit = "Nasi dan tempe oseng"; //--> deklarasi dan inisialisasi
+
+  //menampilkan
+  System.out.println(nama);
+  System.out.println(makananFavorit);
+
+}
+
+}
+  
