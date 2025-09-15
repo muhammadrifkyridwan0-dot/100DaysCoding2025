5b89e13204bc22e143b35a756ef97ef2311347d7
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-16 05:09:57 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+package Rifky;
+
+public class ContohVariabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int x;
+        
+        // Inisialisasi variabel
+        x = 15;
+        System.out.println("Nilai x awal: " + x);
+        
+        // Update variabel
+        x = 30;
+        System.out.println("Nilai x setelah diupdate: " + x);
+    }
+}
