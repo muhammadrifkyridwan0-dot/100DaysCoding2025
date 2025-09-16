a130d5303294c41df9546306b233213480eb1f6b
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-17 00:22:02 +0800

    Update Day10.java

--- Day10.java
+++ Day10.java
@@ -1,10 +1,13 @@
 package Rifky;
-public class ContohFinal {
+public class contohFinal {
 
 public static void main(String[] args) {
         //Final variabel yaitu nilainya tidak dapat diubah setelah diinisialisasi
-    int MAX_SIZE = 10;
-        // Nilai MAX_SIZE = 10; tidak dapat diubah. 
-        System.out.println(MAX_SIZE);
+  final int max = 10;
+        // Nilai max = 10; tidak dapat diubah.
+        System.out.println(max);
+    max = 20;
+        //Error atau tidak dapat diganti
+        System.out.println(max);
     }
 }
