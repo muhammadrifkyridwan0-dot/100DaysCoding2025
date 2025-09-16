ddc369ac338653d8e2959953bd9e4cf060f64c4c
muhammadrifkyridwan0-dot <muhammadrifkyridwan0@gmail.com>
2025-09-17 00:13:44 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+package Rifky;
+public class ContohFinal {
+
+public static void main(String[] args) {
+        //Final variabel yaitu nilainya tidak dapat diubah setelah diinisialisasi
+    int MAX_SIZE = 10;
+        // Nilai MAX_SIZE = 10; tidak dapat diubah. 
+        System.out.println(MAX_SIZE);
+    }
+}
