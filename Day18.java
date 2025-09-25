9768f5442c438fda8641d2e83788d886513516e7
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-09-25 22:41:30 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+package pkg100daycodingisc;
+
+public class Konversi {
+    public static void main(String[] args) {
+        
+        
+        //Konverensi otomatis (Tipe data kecil ke besar)
+        
+        //Wide Cast
+        int a = 19;
+        long b = a;
+        //Secara otomatis a(int) berubah menjadi b(long)
+        System.out.println("int ke long: " + b);
+         
+        
+    }
+}
