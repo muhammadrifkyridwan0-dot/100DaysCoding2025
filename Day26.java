cc9dcb5e7f5c2adc38a0e0b17291267a51186526
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-03 21:25:12 +0800

    Update Day26.java

--- Day26.java
+++ Day26.java
@@ -4,7 +4,7 @@ public class OperatorPenugasan {
 
     public static void main(String[] args) {
         // Deklarasi variabel
-        int a = 10; // operator '=' untuk memberi nilai awal
+        int a = 30; // operator '=' untuk memberi nilai awal
         System.out.println("Nilai awal a = " + a);
 
         // Operator += (penjumlahan dan penugasan)
@@ -12,19 +12,19 @@ public class OperatorPenugasan {
         System.out.println("Setelah a += 5, nilai a = " + a);
 
         // Operator -= (pengurangan dan penugasan)
-        a -= 3; // sama dengan a = a - 3
+        a -= 5; // sama dengan a = a - 3
         System.out.println("Setelah a -= 3, nilai a = " + a);
 
         // Operator *= (perkalian dan penugasan)
-        a *= 2; // sama dengan a = a * 2
+        a *= 4; // sama dengan a = a * 2
         System.out.println("Setelah a *= 2, nilai a = " + a);
 
         // Operator /= (pembagian dan penugasan)
-        a /= 4; // sama dengan a = a / 4
+        a /= 8; // sama dengan a = a / 4
         System.out.println("Setelah a /= 4, nilai a = " + a);
 
         // Operator %= (sisa bagi dan penugasan)
-        a %= 3; // sama dengan a = a % 3
+        a %= 2; // sama dengan a = a % 3
         System.out.println("Setelah a %= 3, nilai a = " + a);
     }
 }
