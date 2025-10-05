0e5fe5d0416e795b0e3ea7b7da3a9873c4a0b17e
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-05 18:14:43 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,25 @@
+package rifky;
+
+public class OperatorPerbandingan {
+    
+    public static void main(String[] args) {
+        int p = 20;
+        int q = 30;
+        int r = 20;
+        
+        //operator ==
+        System.out.println("Apakah p == q ? " + (p == q));
+        System.out.println("Apakah p == r ? " + (p == r));
+
+        // operator !=
+        System.out.println("Apakah p != q ? " + (p != q));
+        System.out.println("Apakah p != r ? " + (p != r));
+        
+
+
+
+
+
+    }
+    
+}
