5718192342ccd37c5e36e40e939e6217f37fdb3e
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-06 08:54:26 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,52 @@
+package rifky;
+
+import java.util.Scanner;
+
+public class OperatorPerbandingan {
+
+    public static void main(String[] args) {
+        
+        // Deklarasi variabel
+        Scanner p = new Scanner (System.in);
+        
+        System.out.print("Masukkan angka: ");
+        int a = p.nextInt();
+        System.out.print("Masukkan angka: ");
+        int b = p.nextInt();
+   
+    
+        //operator >
+        System.out.println("Apakah nilai a lebih besar daripada b? : " + (a > b));
+        System.out.println("Apakah nilai b lebih besar daripada a? : " + (b > a));
+        //operator <
+        System.out.println("Apakah nilai a lebih kecil daripada b? : " + (a < b));
+        System.out.println("Apakah nilai b lebih kecil daripada a? : " + (b < a));
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
