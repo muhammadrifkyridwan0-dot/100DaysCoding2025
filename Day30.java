1a1f16914da14f45fff425ed8f87b883269670cb
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-07 13:40:59 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,13 @@
+package rifky;
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        
+        
+        int a = 75;
+        int b = 40;
+        
+        System.out.println("Apakah nilai a lebih besar atau sama dengan b?: " + (a >= b));
+        System.out.println("Apakah nilai a lebih kecil atau sama dengan b?: " + (a <= b));
+    
+    }
+}
