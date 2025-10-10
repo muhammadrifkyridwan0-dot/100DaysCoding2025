77232d990705fa4ff1bc39107738c18df90b3cf9
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-10 22:59:32 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package rifky;
+
+public class OperatorPerbandingan {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        boolean a = true;        
+
+        System.out.println("Nilai a benar: " + a);
+        System.out.println("Nilai a tidak benar: " + !a);
+}
+}
