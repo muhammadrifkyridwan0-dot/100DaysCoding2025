dc5f36bf5ed3966c667e46245c865926e47d3a41
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-10-28 00:02:57 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,10 @@
+package rifky;
+public class PerulanganFor {
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    
+    }
+}
