a96e5ec760d9a992f8635d8d2f6387abedfdc51c
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-03 20:49:24 +0800

    Day57.java

--- Day57.java
+++ Day57.java
@@ -1,7 +1,7 @@
 package rifky;
 import java.util.Scanner;
 
-public class NestedLoopScanner {
+public class NestedLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan jumlah baris: ");
