6ffeaf32468e7cb2e37818f13a9c4ffa84570736
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-16 21:07:06 +0800

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,21 @@
+package rifky;
+import java.util.Scanner;
+
+public class PolaPersegiPanjang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang: ");
+        int panjang = input.nextInt();
+
+        System.out.print("Masukkan lebar: ");
+        int lebar = input.nextInt();
+
+        for(int i = 1; i <= lebar; i++) {
+            for(int j = 1; j <= panjang; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
