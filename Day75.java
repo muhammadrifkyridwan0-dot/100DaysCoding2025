b3ce26fda936ccb8b41445114be402a9c6319d57
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-21 22:11:54 +0800

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,15 @@
+package rifky;
+import java.util.Scanner;
+public class StringDasar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan sebuah string: ");
+        String kata = input.nextLine();
+        
+        System.out.println("Panjang string: " + teks.length());
+        System.out.println("String Huruf besar: " + teks.toUpperCase());
+        System.out.println("String Huruf kecil: " + teks.toLowerCase());
+        
+    }
+}
