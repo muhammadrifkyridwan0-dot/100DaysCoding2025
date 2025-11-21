3288d003a974447798ceb984fed9d4a10a7f9494
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-21 22:14:11 +0800

    Day75.java

--- Day75.java
+++ Day75.java
@@ -7,9 +7,9 @@ public class StringDasar {
         System.out.print("Masukkan sebuah string: ");
         String kata = input.nextLine();
         
-        System.out.println("Panjang string: " + teks.length());
-        System.out.println("String Huruf besar: " + teks.toUpperCase());
-        System.out.println("String Huruf kecil: " + teks.toLowerCase());
+        System.out.println("Panjang string: " + kata.length());
+        System.out.println("String Huruf besar: " + kata.toUpperCase());
+        System.out.println("String Huruf kecil: " + kata.toLowerCase());
         
     }
 }
