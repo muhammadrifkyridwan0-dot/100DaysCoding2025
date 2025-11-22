66de0157947a9c0427f291d6d6ee9f4eaa5dab58
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-22 22:37:37 +0800

    Create Day76.java

---
+++ Day76.java
@@ -0,0 +1,14 @@
+public class Mengecek {
+    public static void main(String[] args) {
+        String a = "Jawa";
+        String b = "jawa";
+        String c = "Orang Jawa";
+        String kosong = "Ga Ada";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+        System.out.println(ini.contains("Jawa"));
+        System.out.println(kosong.isEmpty());
+    }
+    
+}
