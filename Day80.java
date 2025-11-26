375395e14365edb247d5ddf7bf9a02f432de4862
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-11-26 20:26:59 +0800

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,13 @@
+public class Arrayyy {
+    public static void main(String[] args) {
+
+       
+        int[] angka = {10, 20, 30, 40, 50};
+
+      
+        System.out.println("Isi array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Index " + i + ": " + angka[i]);
+        }
+    }
+}
