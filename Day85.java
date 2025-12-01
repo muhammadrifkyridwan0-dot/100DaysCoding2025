cf9a5a2c15b74b5f53bdc1e2a0e0de9f5ec3e245
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-01 21:24:41 +0800

    Create Day85.java

---
+++ Day85.java
@@ -0,0 +1,26 @@
+package latihanae;
+
+import java.util.Scanner;
+public class MaximalArray {
+
+   public static void main(String[] args) {
+       Scanner p = new Scanner(System.in);
+          System.out.print("Masukkan Angka: ");
+
+          int b p.nextInt();
+          int a[] = new int[b];
+  
+          int max = 0;
+
+          for (int i = 0; i < b; i++) {
+          a[i] = p.nextInt();
+
+          if (b < max || max == 0) {
+          max = a[i];
+
+        }
+
+      }
+      System.out.println("Nilai maksimal: + max);
+    }
+}
