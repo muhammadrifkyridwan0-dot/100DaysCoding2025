ae94f38eb1a03366e670c35f35ccb0f5632b79c4
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-02 21:30:13 +0800

    Day86.java

--- Day86.java
+++ Day86.java
@@ -1,25 +1,26 @@
-package latihanae;
-
 import java.util.Scanner;
-public class MaximalArray {
 
-   public static void main(String[] args) {
-       Scanner p = new Scanner(System.in);
-          System.out.print("Masukkan Angka: ");
+public class MinimalArray {
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
 
-          int b p.nextInt();
-          int a[] = new int[b];
-  
-          int min = a[0];
+        System.out.print("Masukkan jumlah elemen: ");
+        int b = p.nextInt();
+        int a[] = new int[b];
 
-          for (int i = 0; i < b; i++) {
-          a[i] = p.nextInt();
+        for (int i = 0; i < b; i++) {
+            System.out.print("Masukkan elemen ke-" + (i+1) + ": ");
+            a[i] = p.nextInt();
+        }
 
-          if (a[i] < min) {
-              min = a[i];
-          }
-        
-      }
-      System.out.println("Nilai minimal: " + min);
+        int min = a[0];
+
+        for (int i = 1; i < b; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+
+        System.out.println("Nilai minimal: " + min);
     }
 }
