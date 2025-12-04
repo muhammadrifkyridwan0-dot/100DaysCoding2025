ee93699821ee9db1e30ebd8cc4dc78574378ce09
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-04 19:48:28 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,11 @@
+public class AksesArrayForEach {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Isi elemen array:");
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
