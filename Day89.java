b6bd39d9cdcdc1fe345c95319845fd7661b23924
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-05 21:43:57 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,15 @@
+public class MethodVoid{
+
+   
+    public static void contoh(int a, int b) {
+        int hasil = a + b;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+    public static void main(String[] args) {
+
+   
+       contoh(5, 10);
+    }
+}
+       
