893b8e68bb527e7b7a64b8831dc6248e52127f67
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-07 00:16:41 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,12 @@
+public class ParameterMethod{
+
+    public static void amba(String nama) {
+        System.out.println("Halo " + nama);
+    }
+
+    public static void main(String[] args) {
+        amba("Si Imoet");
+        amba("Mas Fuad");
+        amba("Mas Rusdi");
+    }
+}
