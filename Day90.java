e2b8ed07a02afe9acb1dae8a1f3a2893e72b151a
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-06 20:44:26 +0800

    Create Day90.java

---
+++ Day90.java
@@ -0,0 +1,15 @@
+package rifky;
+
+public class MethodNonVoid {
+
+    public static void main(String[] args) {
+
+        System.out.println(penjumlahan(20, 5));
+    }
+
+    public static int penjumlahan(int a, int b) {
+        int hasil = 0;
+
+        return hasil = a + b;
+    }
+}
