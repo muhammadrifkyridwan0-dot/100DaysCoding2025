7691a0bf80c311a9f17a1ffa0099a8dda0443669
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-08 21:20:06 +0800

    Create Day92.java

---
+++ Day92.java
@@ -0,0 +1,12 @@
+public class KeywordReturn{
+
+   
+    static int jumlah(int a, int b) {
+        return a + b;  
+    }
+
+    public static void main(String[] args) {
+        int hasil = jumlah(5, 3);   
+        System.out.println("Hasil penjumlahan = " + hasil);
+    }
+}
