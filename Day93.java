bda3563a99d6083e097c3936525df5d2438f6377
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-09 21:02:17 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,10 @@
+public class MathCeilFloorRound{
+
+public static void main(String[] args) {
+    double nilai = 9.7;
+
+        System.out.println("Ceil  : " + Math.ceil(nilai));
+        System.out.println("Floor : " + Math.floor(nilai));
+        System.out.println("Round : " + Math.round(nilai));
+    }
+}
