496f671210debc24cf905001e2d1c09a595e2df3
Muhammad Rifky Ridwan <muhammadrifkyridwan0@gmail.com>
2025-12-10 15:29:32 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,14 @@
+public class CetakMethodVoid{
+
+   
+    public static void contoh(int a, int b) {
+        int hasil = a + b;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+    public static void main(String[] args) {
+
+   
+       contoh(5, 10);
+    }
+}
